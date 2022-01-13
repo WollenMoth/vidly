5b586e67f01b63fd76a10475bfa2f296129d4513
André Díaz <andre290800@gmail.com>
2022-01-12 20:11:55 -0600

    Managing Development and Production Settings.

---
+++ vidly/settings/dev.py
@@ -0,0 +1,15 @@
+from .common import *
+
+DEGUG = True
+
+SECRET_KEY = 'django-insecure-t(xa99iye#o52216mo24dvo=eu44w7h7ho)qb))t8b+(*yg#h^'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vidly',
+        'HOST': 'mysql',
+        'USER': 'root',
+        'PASSWORD': 'Alitheia2908'
+    }
+}
