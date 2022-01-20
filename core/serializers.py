ae63e21db7057006f36d16e7045d1a036b76b74b
André Díaz <andre290800@gmail.com>
2022-01-20 17:12:35 -0600

    Provide JWT tokens on register.

--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,22 @@
 from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserCreateSerializer
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    def get_token(self, user):
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
     class Meta(BaseUserCreateSerializer.Meta):
         fields = BaseUserCreateSerializer.Meta.fields + \
-            ('first_name', 'last_name')
+            ('first_name', 'last_name', 'token')
 
 
 class UserSerializer(BaseUserSerializer):
