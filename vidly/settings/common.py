77f907bc4b0355c3c87ac56203325bf418238bfe
André Díaz <andre290800@gmail.com>
2022-01-19 18:35:05 -0600

    Getting the Current User.

--- vidly/settings/common.py
+++ vidly/settings/common.py
@@ -136,5 +136,6 @@ AUTH_USER_MODEL = 'core.User'
 DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer',
     }
 }
