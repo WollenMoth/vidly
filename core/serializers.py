dc6b738cac6376281f5719df4c8ff0e0604dace6
André Díaz <andre290800@gmail.com>
2022-01-20 18:02:15 -0600

    Customizing token claims.

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,10 @@
-from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import (
+    UserSerializer as BaseUserSerializer,
+    UserCreateSerializer as BaseUserCreateSerializer,
+)
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer as BaseTokenObtainPairSerializer
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
@@ -8,6 +12,9 @@ class UserCreateSerializer(BaseUserCreateSerializer):
 
     def get_token(self, user):
         refresh = RefreshToken.for_user(user)
+
+        refresh['username'] = user.username
+        refresh['email'] = user.email
 
         return {
             'refresh': str(refresh),
@@ -23,3 +30,14 @@ class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         fields = BaseUserSerializer.Meta.fields + \
             ('first_name', 'last_name')
+
+
+class TokenObtainPairSerializer(BaseTokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        token['email'] = user.email
+
+        return token
--- vidly/urls.py
+++ vidly/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('core.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
