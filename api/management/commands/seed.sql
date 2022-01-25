7165de20a6b4c6240e943e2f3680550aa9baf5ac
André Díaz <andre290800@gmail.com>
2022-01-25 17:00:10 -0600

    Added liked column to seed_db command.

--- api/management/commands/seed.sql
+++ api/management/commands/seed.sql
@@ -7,17 +7,17 @@ values
     (4, 'Thriller');
 
 insert into
-    api_movie (id, title, numberInStock, dailyRentalRate, genre_id)
+    api_movie (id, title, numberInStock, dailyRentalRate, genre_id, liked)
 values
-    (1, 'Airplane', 5, 2, 1),
-    (2, 'The Hangover', 10, 2, 1),
-    (3, 'Wedding Crashers', 15, 2, 1),
-    (4, 'Die Hard', 5, 2, 2),
-    (5, 'Terminator', 10, 2, 2),
-    (6, 'The Avengers', 15, 2, 2),
-    (7, 'The Notebook', 5, 2, 3),
-    (8, 'When Harry Met Sally', 10, 2, 3),
-    (9, 'Pretty Woman', 15, 2, 3),
-    (10, 'The Sixth Sense', 5, 2, 4),
-    (11, 'Gone Girl', 10, 2, 4),
-    (12, 'The Others', 15, 2, 4);
+    (1, 'Airplane', 5, 2, 1, false),
+    (2, 'The Hangover', 10, 2, 1, false),
+    (3, 'Wedding Crashers', 15, 2, 1, false),
+    (4, 'Die Hard', 5, 2, 2, false),
+    (5, 'Terminator', 10, 2, 2, false),
+    (6, 'The Avengers', 15, 2, 2, false),
+    (7, 'The Notebook', 5, 2, 3, false),
+    (8, 'When Harry Met Sally', 10, 2, 3, false),
+    (9, 'Pretty Woman', 15, 2, 3, false),
+    (10, 'The Sixth Sense', 5, 2, 4, false),
+    (11, 'Gone Girl', 10, 2, 4, false),
+    (12, 'The Others', 15, 2, 4, false);
